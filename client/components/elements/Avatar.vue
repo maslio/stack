1bb66460ea3ad716848a793b5fc43f7a91ba6003
maslio <vlad@maslio.com>
2024-08-01 18:14:25 +0700

    feat: create Avatar.vue

---
+++ client/components/elements/Avatar.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+withDefaults(defineProps<{
+  src?: string
+  size: string
+}>(), {
+  size: '20',
+})
+</script>
+
+<template>
+  <div
+    class="relative overflow-hidden rounded-full"
+    :style="{ width: `${size}px`, height: `${size}px` }"
+  >
+    <NuxtImg
+      v-if="src"
+      provider="directus"
+      format="webp"
+      :src="src"
+      :width="size"
+      :height="size"
+    />
+    <div v-else>
+      <NuxtIcon
+        class="text-neutral"
+        name="material-symbols:account-circle"
+        :size
+      />
+    </div>
+  </div>
+</template>

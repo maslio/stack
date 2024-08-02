2981b3456e42edc870586194bdba9a436abd3238
maslio <vlad@maslio.com>
2024-08-02 18:21:21 +0700

    feat: create v-fast-click directive

--- client/plugins/click.ts
+++ client/plugins/click.ts
@@ -16,4 +16,23 @@ export default defineNuxtPlugin((nuxtApp) => {
       })
     },
   })
+  nuxtApp.vueApp.directive('fast-click', {
+    mounted(el) {
+      el.addEventListener('mousedown', (e: Event) => {
+        if (e instanceof MouseEvent && e.button === 0) {
+          const target = e.target as HTMLElement
+          if (target.tagName === 'TEXTAREA' || target.tagName === 'INPUT')
+            return
+          e.preventDefault()
+          e.stopPropagation()
+          el.dispatchEvent(new Event('click', { bubbles: true }))
+        }
+      })
+      // el.addEventListener('click', (e: Event) => {
+      //   // @ts-expect-error click from keyboard enter key
+      //   // if (!e.pointerType)
+      //   //   binding.value?.(e)
+      // })
+    },
+  })
 })
